17083059ac6569536dfcd6f18e426000e9b249e8
anhyoseung <ace.aaiden@gmail.com>
2023-02-03 10:46:43 +0900

    timer setInterval 사용 (큰 초 차이는 없음)-에이든

--- public/timer.js
+++ public/timer.js
@@ -1,9 +1,9 @@
 function makeTimer() {
-  // let endTime = new Date('2 February 2023 19:25:00 GMT+09:00')
+  //GMT 09:00한국 기준
   let endTime = new Date('4 February 2023 13:00:00 GMT+09:00')
 
-  //GMT 09:00한국 기준
-  // let endTime = new Date('2 February 2023 08:43:00 GMT+01:00')
+  //미국 서부(뉴욕쪽) pst기준
+  // let endTime = new Date('3 February 2023 20:00:00 GMT-08:00')
 
   endTime = (Date.parse(endTime) / 1000)
 
@@ -16,35 +16,40 @@ function makeTimer() {
   let hours = Math.floor((timeLeft - (days * 86400)) / 3600)
   let minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60)
   let seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)))
+
+  // ✅설치 가능 alert
   if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
 
     alert('🚀 Downloading available!!!')
-    //날짜도 여기로
   }
+  // ✅기간 지나면 'Downloading text로
   else if (days < 0) {
+    clearInterval(x);
+    console.log('done?')
     $('#Downloaoding').html('<h3>Downloaoding !</h3>')
     $('.days').html('')
     $('.hours').html('')
     $('.minutes').html('')
-    $('.seconds').html()
+    $('.seconds').html('')
   }
+  // ✅ 카운팅 ( 00:00:00 ) 작업
   else {
-      if (hours < '10') {
-        hours = '0' + hours
-      }
-      if (minutes < '10') {
-        minutes = '0' + minutes
-      }
-      if (seconds < '10') {
-        seconds = '0' + seconds
-      }
-      $('.days').html(days + '<span>Days</span>')
-      $('.hours').html(hours + '<span>:</span>')
-      $('.minutes').html(minutes + '<span>:</span>')
-      $('.seconds').html(seconds)
+    if (hours < '10') {
+      hours = '0' + hours
+    }
+    if (minutes < '10') {
+      minutes = '0' + minutes
+    }
+    if (seconds < '10') {
+      seconds = '0' + seconds
+    }
+    $('.days').html(`<span>Download start timer : ${days} Days</span>`)
+    $('.hours').html(hours + '<span>:</span>')
+    $('.minutes').html(minutes + '<span>:</span>')
+    $('.seconds').html(seconds)
   }
 }
 
-setInterval(function() {
+x = setInterval(function() {
   makeTimer()
 }, 1000)
